9bd829a9cd1453a491fc4f20f2110f0859de1925
matthieu661 <matthieu.bois@outlook.fr>
2021-06-22 19:24:59 +0200

    POSTMAN des 5 routes basiques implantées

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
 const express = require('express');
 
 
+
 const app = express();
 
+// utilitaire object
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 // routes const :
 // route use :
@@ -11,27 +16,8 @@ const apiRoutes = require('./routes/apiRoutes');
 app.use("/api", apiRoutes);
 
 
-/*le premier enregistre « Requête reçue ! »
- dans la console et passe l'exécution ;*/
-app.use((req, res, next) => {
-  console.log('Requête reçue !');
-  next();
-});
-/*le deuxième ajoute un code d'état 201 à la réponse
- et passe l'exécution ;*/
-app.use((req, res, next) => {
-  res.status(201);
-  next();
-});
-/*le troisième envoie la réponse JSON et passe l'exécution ;*/
-app.use((req, res, next) => {
-  res.json({ message: 'Votre requête a bien été reçue !' });
-  next();
-});
-/*le dernier élément de middleware enregistre
- « Réponse envoyée avec succès ! » dans la console.*/
-app.use((req, res, next) => {
-  console.log('Réponse envoyée avec succès !');
-});
+
+
+
 
 module.exports = app;
--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,10 +1,45 @@
 const express = require('express'); // autoconvertion vers ES6
 const router = express.Router();
-const db = require("../models/index");
+const db = require("../models");
 
-
+//GET ALL
 router.get("/all", (req, res) => {
     db.Todo.findAll().then(todos => res.send(todos));
-} );
+});
+// CREATE ONE
+router.post("/new", (req, res) => {
+    db.Todo.create({
+        text: req.body.text,
+    }).then(X => res.send(X));
+});
+//  GET ONE
+router.get("/find/:id", (req, res) => {
+    db.Todo.findAll({
+        where: {
+            id: req.params.id
+        }
+    }).then(X => res.send(X));
+})
+// DELETE ONE 
+router.delete("/delete/:id", (req, res) => {
+    db.Todo.destroy({
+        where: {
+            id: req.params.id
+        }
+    }).then(() => res.send('todo est mort !'))
+})
+// MODIFY ONE 
+router.put("/modify", (req, res) => {
+    db.Todo.update(
+        {
+            text: req.body.text
+        },
+        {
+            where: {
+                id : req.body.id
 
-module.exports = router; 
+            }
+        }).then(() => res.send("todo à changé de style"));
+})
+
+module.exports = router;
